defb02aa5e3e1eb20e776695c5ec95e9efd2e64d
FelipeALuna <felipeluna_2000@hotmail.com>
2021-08-30 14:31:20 -0300

    exibindo contatos na tela

--- src/app/components/contato/contato.component.ts
+++ src/app/components/contato/contato.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Contato } from 'src/app/models/Contato';
+import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-contato',
   templateUrl: './contato.component.html',
   styleUrls: ['./contato.component.css']
 })
 export class ContatoComponent implements OnInit {
-
+  @Input() contato!: Contato;
   constructor() { }
 
   ngOnInit(): void {
--- src/app/components/lista-de-contatos/lista-de-contatos.component.ts
+++ src/app/components/lista-de-contatos/lista-de-contatos.component.ts
@@ -1,3 +1,4 @@
+import { Contato } from 'src/app/models/Contato';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,7 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lista-de-contatos.component.css']
 })
 export class ListaDeContatosComponent implements OnInit {
-
+  contatos: Contato[] = [
+    {
+      nome: "Felipe",
+      email: "felipeluna_2000@hotmail.com",
+      telefones: ["99195-2757"]
+    },
+    {
+      nome: "Jão",
+      email: "felipeluna_2000@hotmail.com",
+      telefones: ["99195-2757"]
+    },
+    {
+      nome: "Marcos",
+      email: "felipeluna_2000@hotmail.com",
+      telefones: ["99195-2757"]
+    },
+    {
+      nome: "Junior",
+      email: "felipeluna_2000@hotmail.com",
+      telefones: ["99195-2757"]
+    },
+    {
+      nome: "Carlin",
+      email: "felipeluna_2000@hotmail.com",
+      telefones: ["99195-2757"]
+    },
+  ];
   constructor() { }
 
   ngOnInit(): void {
