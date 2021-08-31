85b4c4a368f9810af79be8584032a41341ada1c4
FelipeALuna <felipeluna_2000@hotmail.com>
2021-08-31 10:19:21 -0300

    criado o serviço manipulação de contatos

--- src/app/components/lista-de-contatos/lista-de-contatos.component.ts
+++ src/app/components/lista-de-contatos/lista-de-contatos.component.ts
@@ -1,5 +1,6 @@
 import { Contato } from 'src/app/models/Contato';
 import { Component, OnInit } from '@angular/core';
+import { ContatoService } from 'src/app/services/contato.service';
 
 @Component({
   selector: 'app-lista-de-contatos',
@@ -7,36 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lista-de-contatos.component.css']
 })
 export class ListaDeContatosComponent implements OnInit {
-  contatos: Contato[] = [
-    {
-      nome: "Felipe",
-      email: "felipeluna_2000@hotmail.com",
-      telefones: ["99195-2757"]
-    },
-    {
-      nome: "Jão",
-      email: "felipeluna_2000@hotmail.com",
-      telefones: ["99195-2757","38462306"]
-    },
-    {
-      nome: "Marcos",
-      email: "felipeluna_2000@hotmail.com",
-      telefones: ["99195-2757"]
-    },
-    {
-      nome: "Junior",
-      email: "felipeluna_2000@hotmail.com",
-      telefones: ["99195-2757"]
-    },
-    {
-      nome: "Carlin",
-      email: "felipeluna_2000@hotmail.com",
-      telefones: ["99195-2757"]
-    },
-  ];
-  constructor() { }
+  contatos: Contato[] = [];
+  private cs:ContatoService = new ContatoService();
+  constructor() {
+    this.contatos = this.cs.getContatos();
+   }
 
   ngOnInit(): void {
   }
+
 
 }
